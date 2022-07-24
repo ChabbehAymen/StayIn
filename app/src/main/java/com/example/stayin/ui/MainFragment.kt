3c3db79db4d973732a6561fb946bc32352423568
ChabbehAymen <devaymen15.5.3@gmail.com>
2022-07-24 23:10:49 +0100

    commit

---
+++ app/src/main/java/com/example/stayin/ui/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.stayin.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.stayin.R
+import com.example.stayin.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+}
