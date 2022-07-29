ae236b37c92d883bd538841adc664a475c295170
ChabbehAymen <devaymen15.5.3@gmail.com>
2022-07-29 20:46:35 +0100

    commit

--- app/src/main/java/com/example/stayin/ui/MainActivity.kt
+++ app/src/main/java/com/example/stayin/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.stayin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.stayin.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getNavController()
+    }
+
+    private fun getNavHostFragment(): NavHostFragment {
+        return supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    private fun getNavController(): NavController{
+        return getNavHostFragment().navController
     }
 }
