bb03dbf34dba6601ac4c240056617d096bbfea1c
ChabbehAymen <devaymen15.5.3@gmail.com>
2022-09-10 18:14:32 +0100

    progress on

--- app/src/main/java/com/example/stayin/ui/EditFragment.kt
+++ app/src/main/java/com/example/stayin/ui/EditFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 import com.example.stayin.R
 import com.example.stayin.databinding.FragmentEditBinding
 
@@ -20,6 +22,28 @@ class EditFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentEditBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onBackButtonClick(binding.navigateBackImageButton)
+    }
+
+    private fun onBackButtonClick(backButton: ImageButton){
+        backButton.setOnClickListener {
+            navigateToMainFragment()
+        }
+    }
+
+    private fun navigateToMainFragment(){
+        val action = R.id.action_editFragment_to_mainFragment
+        findNavController().navigate(action)
+    }
+
+    private fun onShowDesignDialog(arrowUpButton: ImageButton){
+        arrowUpButton.setOnClickListener {
+            // show design dialog fragment
+        }
     }
 
 }
