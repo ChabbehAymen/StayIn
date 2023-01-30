8a1f0582e96fc1995d328231af070d89586dbea7
Aymen Chabbeh <chebbehaymen@gmai.com>
2023-01-30 16:26:35 +0100

    removing the DI and hilt from the project motivated by:"make it work than make it beautiful"_uncle bob

---
+++ app/src/main/java/com/example/stayin/presentation/ui/SharedViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.stayin.presentation.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.stayin.useCases.NoteUseCase
+
+class SharedViewModelFactory(private val noteUseCase: NoteUseCase): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(SharedViewModel::class.java))
+            SharedViewModel(noteUseCase) as T
+        else
+            throw IllegalAccessException("Unknown class ")
+    }
+}
